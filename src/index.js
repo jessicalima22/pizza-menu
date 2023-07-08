952f2610d9435af7e284f9966f1a8948d7c96ca4
jessicalima22 <jessica.lima22@hotmail.com>
2023-07-07 23:38:07 -0300

    using ternary

--- src/index.js
+++ src/index.js
@@ -75,12 +75,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working in our menu. Please come back later =]</p>
       )}
 
       <Order />
